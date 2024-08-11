fe9219f14ec66176a65a061fe4d667a664a38284
KublS <kublitskyi@gmail.com>
2024-08-11 09:56:31 -0400

    New changes in Class6

--- src/test/java/Git1/kublitskyi/kublitskyiClass6.java
+++ src/test/java/Git1/kublitskyi/kublitskyiClass6.java
@@ -4,5 +4,9 @@ public class kublitskyiClass6 {
     public static void main(String[] args) {
 
         System.out.println("New Changes");
+
+        System.out.println("Extra line before merge");
     }
+
+
 }
