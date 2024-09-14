a3a6240bfd41d5ff18af42012c9f467c9d0855d2
KublS <kublitskyi@gmail.com>
2024-09-14 12:17:11 -0400

    countYZ

---
+++ src/test/java/Git1/kublitskyi/kublitskyiInterview.java
@@ -0,0 +1,32 @@
+package Git1.kublitskyi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class kublitskyiInterview {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print any string");
+        String input = scanner.nextLine();
+        scanner.close();
+        System.out.println("Answer:");
+        System.out.println(numberOfEndings(input));
+    }
+
+    public static int numberOfEndings(String input){
+        String[] inputArray = input.split(" ");
+        int counter=0;
+        for (String eachString : inputArray) {
+            int lastSymbol = eachString.length()-1;
+            char[] each = eachString.toLowerCase().toCharArray();
+            if(each[lastSymbol]=='y' || each[lastSymbol]=='z'){
+                counter+=1;
+            }
+
+        }
+        return counter;
+    }
+}
